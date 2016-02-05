ca7c026eebd3f4e3984cf4d010d039f72fc4be6d
Zumium <martin007323@gmail.com>
2016-02-05 14:30:53 +0800

    fix bug -- expand ~ to real user home path

--- boxes/configParse.py
+++ boxes/configParse.py
@@ -1,8 +1,10 @@
 import configparser
 import os
+import os.path
 
 def getConfigPath():
 	configPath=['~/.boxesrc','/usr/local/etc/boxes/boxesrc','/etc/boxes/boxesrc']
+	os.path.expanduser(configPath[0])
 	config=None
 	for confPath in configPath:
 		if os.path.isfile(confPath):
